4fd72acea1578736a39147df6b57e259a9071e51
sahiljena <sahiljena@protonmail.com>
2021-11-22 17:03:53 +0530

    Home section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,19 +2,24 @@ import React from 'react';
 import {ReactComponent as FrontLandingPageImage} from '../images/undraw_knowledge_g-5-gf.svg'
 const Home = () =>{
     return (
-        <main>
-        <section className="introduction">
-            <div className="text-part">
-            <h1>
-                aeroSpect<br />
-                Education <span className="success">Redifened</span>
-            </h1>
-            <h4>With our <span className="danger">3D</span> responsive educative models <span className="primary">everyone</span> can learn</h4>
-            </div>
-            <div className="img-holder">
-            <img src="https://i.ibb.co/MC4DjBQ/undraw-knowledge-g5gf.png" />
-            </div>
-        </section>
+        <main className="main">
+            <section className="introduction">
+                <div className="flex-jc-sa">
+                    <div className="text-part">
+                        <h1>
+                            aeroSpect<br />
+                            Education <span className="success">Redifened</span>
+                        </h1>
+                        <h4>With our <span className="danger">3D</span> responsive educative models <span className="primary">everyone</span> can learn</h4>
+                        <div className="searchBox">
+                                <button className="primary-btn">Explore</button>
+                        </div>
+                    </div>
+                        <div className="img-holder">
+                        <img src="https://i.ibb.co/MC4DjBQ/undraw-knowledge-g5gf.png" />
+                    </div>
+                </div>
+            </section>
         </main>
     );
 }
