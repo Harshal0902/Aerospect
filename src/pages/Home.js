3f8bcf31a9cac6059d22b413af10af98a4147462
sahiljena <sahiljena@protonmail.com>
2021-11-22 16:41:06 +0530

    Added home section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import {ReactComponent as FrontLandingPageImage} from '../images/undraw_knowledge_g-5-gf.svg'
 const Home = () =>{
     return (
-        <div>
-        This is Home
-        </div>
+        <main>
+        <section class="introduction">
+            <div class="text-part">
+            <h1>
+                aeroSpect<br />
+                Education <span className="success">Redifened</span>
+            </h1>
+            <h4>With our 3D responsive educative models everyone can learn</h4>
+            </div>
+            <div class="img-holder">
+            <img src="https://i.ibb.co/MC4DjBQ/undraw-knowledge-g5gf.png" />
+            </div>
+        </section>
+        </main>
     );
 }
 export default Home;
